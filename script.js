0d27b7543c8819eb5cbd360abc99c633b98d2a35
KEERTHANADURAISAM <115733037+KEERTHANADURAISAM@users.noreply.github.com>
2022-12-28 13:42:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+ let output=document.getElementById('output')
+ function display(Num){
+    output.value+=Num;
+ }
+ function equal(){
+    try{
+    output.value=eval(output.value);
+ }
+ catch(err){
+alert("invalid")
+ }
+}
+function Clear(){
+    output.value='';
+}
+
+ function del(){
+    output.value=output.value.slice(0,-1);
+ }
